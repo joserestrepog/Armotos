3066c584accd132daf5db1385bb50b3687e63cd9
Jose Luis Restrepo Gesamá <jose1.restrepo@ucp.edu.co>
2023-02-23 15:13:52 -0500

    Actualizar Servicio funcional.

--- Controlador/ControladorProducto/ControllerUpdateProducto.php
+++ Controlador/ControladorProducto/ControllerUpdateProducto.php
@@ -1,27 +1,28 @@
 <?php
-     $codigo=$_POST['codigoBuscar']; 
+     $placa=$_POST['placa'];
+     $nombre=$_POST['nombre'];     
+     $fechaing=$_POST['fechaing'];  
+     $fechasal=$_POST['fechasal'];  
+     $usuario=$_POST['usuario'];
      $descripcion=$_POST['descripcion']; 
-     $precio=$_POST['precio']; 
-     $cantidad=$_POST['cantidad']; 
-     $fecha_ingreso=$_POST['fechaIngreso']; 
-     $fecha_vencimiento=$_POST['fechaVencimiento']; 
+     $precio=$_POST['precio'];
      include '../../Modelo/Servicios.php';
      
-     $result=editarProducto($codigo, $descripcion, $precio, $cantidad, $fecha_ingreso, $fecha_vencimiento);
-     if($result=="OK"){
+     $result=editarServicio($placa, $nombre, $fechaing, $fechasal, $usuario, $descripcion, $precio);
+     if($result=="true"){
         echo <<<JAVASCRIPT
         <script type="text/javascript">
-            alert("Se actualizó el producto.");
+            alert("Se actualizó el servicio.");
         </script>
         JAVASCRIPT;
-        header ("refresh:0;url=../../Vista\Modulo_administrador\html\Inventario\ModuloInventarioActualizar.html"); 
+        header ("refresh:0;url=../../Vista/Modulo_administrador/html/ModuloGestionActualizar.html");  
      }
-     else if($result=="ERROR"){
+     else if($result=="false"){
         echo <<<JAVASCRIPT
         <script type="text/javascript">
-            alert("Error al actualizar el producto.");
+            alert("Error al actualizar el servicio.");
         </script>
         JAVASCRIPT;
-        header ("refresh:0;url=../../Vista\Modulo_administrador\html\Inventario\ModuloInventarioActualizar.html"); 
+        header ("refresh:0;url=../../Vista/Modulo_administrador/html/ModuloGestionActualizar.html");  
      }
 ?>
--- Modelo/Servicios.php
+++ Modelo/Servicios.php
@@ -10,8 +10,7 @@
                 $res="true";
             }else{
                 $res="false";
-            }
-            
+            }            
         return $res;
         }
 
@@ -28,20 +27,51 @@
             return $res;
         }
 
-        function editarProducto($codigo, $descripcion, $precio, $cantidad, $fecha_ingreso, $fecha_vencimiento){
+        function editarServicio($placa, $nombre, $fechaing, $fechasal, $usuario, $descripcion, $precio){
             include 'conexion.php';
             $res="";
-            $query = "SELECT id_producto FROM $schema.producto WHERE id_producto=$codigo";
+            $query = "SELECT id_servicio FROM servicio WHERE placa='$placa' ORDER BY fecha_ingreso DESC";
+            try{
+                $sql = mysqli_query($conn,$query);                
+                $filas = mysqli_num_rows($sql); 
+                if($filas != 1){
+                    while($row = mysqli_fetch_array($sql)){
+                        $id_servicio = $row['id_servicio'];                    
+                        break;
+                    }
+                    $query2 = "UPDATE servicio SET placa='$placa', nombre='$nombre', fecha_ingreso='$fechaing', fecha_salida='$fechasal', fk_usuario='$usuario', descripcion='$descripcion', precio=$precio WHERE id_servicio=$id_servicio";
+                    try{
+                        $sq2l = mysqli_query($conn,$query2);
+                        $res="true";      
+                    }catch (Exception $e){
+                        $res="false";
+                    }                    
+                }else{
+                    $res="false";
+                }               
+            }catch (Exception $e) {
+                $res="false";
+            }            
+            return $res;            
+        }
+        
+        function buscarProducto($codigo){
+            include 'conexion.php';
+            $res=[];           
+            $query = "SELECT descripcion, precio, cantidad, fecha_ingreso, fecha_vencimiento FROM $schema.producto WHERE id_producto=$codigo";
             $sql = pg_query($conn,$query);
-            if(pg_num_rows($sql) == 0){
-                $res="ERROR";
-            }else if(pg_num_rows($sql) == 1){
-                $query2 = "UPDATE $schema.producto SET id_producto=$codigo, descripcion='$descripcion', precio=$precio, cantidad=$cantidad, fecha_ingreso='$fecha_ingreso', fecha_vencimiento='$fecha_vencimiento' WHERE id_producto=$codigo";
-                $sql = pg_query($conn,$query2);
-                $res="OK";
+            while($Row=pg_fetch_assoc($sql)){
+                $res[0]=$Row['descripcion'];
+                $res[1]=$Row['precio'];
+                $res[2]=$Row['cantidad'];
+                $res[3]=$Row['fecha_ingreso'];
+                $res[4]=$Row['fecha_vencimiento'];
             }
-            return $res;
-        }        
+            if($res==NULL){
+                $res="ERROR";
+            }          
+            return $res; 
+        }
         
         function eliminarProducto($codigo){
             include 'conexion.php';
@@ -58,5 +88,5 @@
                  
             }
             return $res;
-        }
+        }        
 ?>
